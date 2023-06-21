5006f6826d7fba0229d44deb69f302673d097fe9
mchoi-cmd <123826506+mchoi-cmd@users.noreply.github.com>
2023-06-21 10:01:14 -0700

    Update nodejs server example
    add a few more things to try.

--- playground/nodejs/server_example/server.js
+++ playground/nodejs/server_example/server.js
@@ -19,6 +19,27 @@ app.get('/api/user/:id', (req, res) => {
   res.json({ id: userId, name: 'John Doe' });
 });
 
+app.get('/api/address/:id', (req, res) => {
+  const addressId = req.params.id;
+
+  // Simulating an error for demonstration purposes
+  if (addressId === 'error') {
+    const error = new Error('Address not found');
+    // error.status = 404;
+    throw error;
+  }
+
+  // Simulating a successful response
+  res.json({ id: addressId, name: '123 Spring Ave' });
+});
+
+app.post('/api/address/:id', (req, res) => {
+  const addressId = req.params.id;
+
+  // Simulating a successful response
+  res.json({id: addressId})
+});
+
 // Error handling middleware
 app.use((err, req, res, next) => {
   // Default to 500 Internal Server Error if status code is not set
@@ -26,6 +47,8 @@ app.use((err, req, res, next) => {
 
   // Log the error for debugging purposes
   console.error(err);
+  console.log("Error Handling Middleware called")
+  console.log('Path: ', req.path)
 
   // Send an error response
   res.status(statusCode).json({ error: err.message });
--- playground/nodejs/server_example/test_server.py
+++ playground/nodejs/server_example/test_server.py
@@ -1,20 +1,47 @@
 import requests
 
 base_url = "http://localhost:3000"
+sample_name = 'john'
+sample_address = '123 ABC street'
 
-def test_get_request_positive():
+def test_get_request_user_positive():
     # Send a GET request to the API
-    response = requests.get(base_url + "/api/user/john")
+    response = requests.get(base_url + "/api/user/" + sample_name)
 
     # Assert the response status code
     assert response.status_code == 200
-    assert response.json()['id'] == 'john'
+    assert response.json()['id'] == sample_name
     assert response.json()['name'] == 'John Doe'
 
-def test_get_request_negative():
+def test_get_request_user_negative():
     # Send a GET request to the API
     response = requests.get(base_url + "/api/user/error")
     
     # Assert the response status code
     assert response.status_code == 404
     assert response.json()['error'] == 'User not found'
+
+def test_get_request_address_positive():
+    # Send a GET request to the API
+    response = requests.get(base_url + "/api/address/" + sample_address)
+
+    # Assert the response status code
+    assert response.status_code == 200
+    assert response.json()['id'] == sample_address
+    assert response.json()['name'] == '123 Spring Ave'
+
+def test_get_request_address_negative():
+    # Send a GET request to the API
+    response = requests.get(base_url + "/api/address/error")
+    
+    # Assert the response status code
+    assert response.status_code == 500
+    assert response.json()['error'] == 'Address not found'
+
+def test_post_request_address():
+    # Send a POST request to the API
+    response = requests.post(base_url + "/api/address/" + sample_address)
+
+    # Assert the response status code
+    assert response.status_code == 200
+    assert response.json()['id'] == sample_address
