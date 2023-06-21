fec69232c65ccd5e9cea5f1a19f20fc42abd21c6
mchoi-cmd <123826506+mchoi-cmd@users.noreply.github.com>
2023-06-21 11:13:20 -0700

    Update server.js
    npx standard --fix

--- playground/nodejs/server_example/server.js
+++ playground/nodejs/server_example/server.js
@@ -1,58 +1,58 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
-app.use(express.json());
+app.use(express.json())
 
 // Example route that handles a GET request
 app.get('/api/user/:id', (req, res) => {
-  const userId = req.params.id;
+  const userId = req.params.id
 
   // Simulating an error for demonstration purposes
   if (userId === 'error') {
-    const error = new Error('User not found');
-    error.status = 404;
-    throw error;
+    const error = new Error('User not found')
+    error.status = 404
+    throw error
   }
 
   // Simulating a successful response
-  res.json({ id: userId, name: 'John Doe' });
-});
+  res.json({ id: userId, name: 'John Doe' })
+})
 
 app.get('/api/address/:id', (req, res) => {
-  const addressId = req.params.id;
+  const addressId = req.params.id
 
   // Simulating an error for demonstration purposes
   if (addressId === 'error') {
-    const error = new Error('Address not found');
-    throw error;
+    const error = new Error('Address not found')
+    throw error
   }
 
   // Simulating a successful response
-  res.json({ id: addressId, name: '123 Spring Ave' });
-});
+  res.json({ id: addressId, name: '123 Spring Ave' })
+})
 
 app.post('/api/address/:id', (req, res) => {
-  const addressId = req.params.id;
+  const addressId = req.params.id
 
   // Simulating a successful response
-  res.json({id: addressId})
-});
+  res.json({ id: addressId })
+})
 
 // Error handling middleware
 app.use((err, req, res, next) => {
   // Default to 500 Internal Server Error if status code is not set
-  const statusCode = err.status || 500;
+  const statusCode = err.status || 500
 
   // Log the error for debugging purposes
-  console.error(err);
-  console.log("Error Handling Middleware called")
+  console.error(err)
+  console.log('Error Handling Middleware called')
   console.log('Path: ', req.path)
 
   // Send an error response
-  res.status(statusCode).json({ error: err.message });
-});
+  res.status(statusCode).json({ error: err.message })
+})
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+  console.log(`Server listening on port ${port}`)
+})
