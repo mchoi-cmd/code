928b3a2bb93040b907edb3d866b5f282a9866335
mchoi-cmd <123826506+mchoi-cmd@users.noreply.github.com>
2023-06-21 14:44:29 -0700

    Update server.js

--- playground/nodejs/t2assessment/server.js
+++ playground/nodejs/t2assessment/server.js
@@ -1,62 +1,62 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.post('/cart', (req, res) => {
   // Validate the request body
   if (!req.body.items || !Array.isArray(req.body.items)) {
-    return res.status(400).json({ error: 'Invalid request body' });
+    return res.status(400).json({ error: 'Invalid request body' })
   }
 
   if (req.body.items.length === 0) {
-    return res.status(400).json({ error: 'No item in cart' });
+    return res.status(400).json({ error: 'No item in cart' })
   }
 
   // Calculate the total price
   const cartItems = req.body.items.map(item => {
     if (!item.id || typeof item.unitPrice !== 'number' || typeof item.quantity !== 'number') {
-      return res.status(400).json({ error: 'Invalid item in the request body' });
+      return res.status(400).json({ error: 'Invalid item in the request body' })
     }
     if (item.unitPrice < 0) {
-      return res.status(400).json({ error: 'Unit Price is less than zero' });
+      return res.status(400).json({ error: 'Unit Price is less than zero' })
     }
     if (item.quantity < 0) {
-      return res.status(400).json({ error: 'Quantity is less than zero' });
+      return res.status(400).json({ error: 'Quantity is less than zero' })
     }
-    return item;
-  });
-  const totalPrice = calculateTotalPrice(req.body.items);
+    return item
+  })
+  const totalPrice = calculateTotalPrice(req.body.items)
 
   // Prepare the response
   const response = {
     items: cartItems,
-    totalPrice: totalPrice
-  };
+    totalPrice
+  }
 
   // Send the response
-  res.json(response);
-});
+  res.json(response)
+})
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: 'Something went wrong!' });
-});
+  console.error(err.stack)
+  res.status(500).json({ error: 'Something went wrong!' })
+})
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  console.log(`Server is running on port ${port}`)
+})
 
-function calculateTotalPrice(items) {
-  let totalPrice = 0;
+function calculateTotalPrice (items) {
+  let totalPrice = 0
   items.map(item => {
-    totalPrice += item.unitPrice * item.quantity;
-  });
+    totalPrice += item.unitPrice * item.quantity
+  })
 
   return totalPrice
 }
