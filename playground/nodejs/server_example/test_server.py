28ff75f86fa0516e5cf06e1eac40f65026e3d6e1
mchoi-cmd <123826506+mchoi-cmd@users.noreply.github.com>
2023-06-21 11:26:20 -0700

    Update test_server.py

--- playground/nodejs/server_example/test_server.py
+++ playground/nodejs/server_example/test_server.py
@@ -1,8 +1,8 @@
 import requests
 
 base_url = "http://localhost:3000"
-sample_name = 'john'
-sample_address = '123 ABC street'
+sample_name = "john"
+sample_address = "123 ABC street"
 
 def test_get_request_user_positive():
     # Send a GET request to the API
@@ -10,16 +10,16 @@ def test_get_request_user_positive():
 
     # Assert the response status code
     assert response.status_code == 200
-    assert response.json()['id'] == sample_name
-    assert response.json()['name'] == 'John Doe'
+    assert response.json()["id"] == sample_name
+    assert response.json()["name"] == "John Doe"
 
 def test_get_request_user_negative():
     # Send a GET request to the API
     response = requests.get(base_url + "/api/user/error")
-    
+
     # Assert the response status code
     assert response.status_code == 404
-    assert response.json()['error'] == 'User not found'
+    assert response.json()["error"] == "User not found"
 
 def test_get_request_address_positive():
     # Send a GET request to the API
@@ -27,16 +27,16 @@ def test_get_request_address_positive():
 
     # Assert the response status code
     assert response.status_code == 200
-    assert response.json()['id'] == sample_address
-    assert response.json()['name'] == '123 Spring Ave'
+    assert response.json()["id"] == sample_address
+    assert response.json()["name"] == "123 Spring Ave"
 
 def test_get_request_address_negative():
     # Send a GET request to the API
     response = requests.get(base_url + "/api/address/error")
-    
+
     # Assert the response status code
     assert response.status_code == 500
-    assert response.json()['error'] == 'Address not found'
+    assert response.json()["error"] == "Address not found"
 
 def test_post_request_address():
     # Send a POST request to the API
@@ -44,4 +44,4 @@ def test_post_request_address():
 
     # Assert the response status code
     assert response.status_code == 200
-    assert response.json()['id'] == sample_address
+    assert response.json()["id"] == sample_address
