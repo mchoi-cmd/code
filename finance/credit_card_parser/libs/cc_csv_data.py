e65b2fb46eac2ea386709dd064a72c539ab870d5
mchoi-cmd <123826506+mchoi-cmd@users.noreply.github.com>
2025-02-12 21:25:38 -0800

    feat: update to include bmo

--- finance/credit_card_parser/libs/cc_csv_data.py
+++ finance/credit_card_parser/libs/cc_csv_data.py
@@ -1,22 +1,27 @@
 # Copyright Michael Choi All Rights Reserved
+"""module containing class for credit card csv data"""
 
+class DataCreditCard:
+    """class representing generic credit card data"""
 
-class data_credit_card:
     def __init__(self, posted_date, description, amount):
         self.posted_date = posted_date
         self.description = description
-        self.amount = amount
+        self.amount = amount.replace(',', '')
         self.purchase = False
 
-    def __str__(self):
-        return self.posted_date + "," + self.description + "," + self.amount
-
     def display(self):
-        output_string = self.__str__(self)
+        """display posted date, description and amount"""
+        output_string = self.posted_date + "," + self.description + "," + self.amount
         return output_string
 
+    def is_purchase(self):
+        """check if the transaction is a purchase"""
+        return self.purchase
 
-class data_brim(data_credit_card):
+
+class DataBrim(DataCreditCard):
+    """class representing BRIM credit card data"""
     # BRIM columns
     # 0 ['No',
     # 1 'Transaction Date',
@@ -28,19 +33,22 @@ class data_brim(data_credit_card):
     payment_filter = ["Redemption", "Refund", "Payment"]
 
     def __init__(self, line):
-        data_credit_card.__init__(self, line[2], line[3], line[4])
+        DataCreditCard.__init__(self, line[2], line[3], line[4])
         self.transaction_date = line[1]
         self.category = line[6]
-        if self.category not in self.payment_filter:
-            self.purchase = True
+        self.purchase = self.category not in self.payment_filter
+        # if self.category not in self.payment_filter:
+        #     self.purchase = True
 
     @classmethod
-    def initialize(self, line):
-        self.__init__(self, line)
-        return self
+    def initialize(cls, line):
+        """public method to initialize the class"""
+        cls.__init__(cls, line)
+        return cls
 
 
-class data_mbna(data_credit_card):
+class DataMbna(DataCreditCard):
+    """class representing MBNA credit card data"""
     # MBNA columns
     # 0 Posted Date,
     # 1 Payee,
@@ -49,20 +57,21 @@ class data_mbna(data_credit_card):
     payment_filter = ["CASH REWARD", "PAYMENT"]
 
     def __init__(self, line):
-        data_credit_card.__init__(self, line[0], line[1], line[3])
-        if self.description not in self.payment_filter:
-            self.purchase = True
+        DataCreditCard.__init__(self, line[0], line[1], line[3])
+        self.purchase = self.description not in self.payment_filter
 
     def __str__(self):
         return self.posted_date + "," + self.description + "," + self.amount.replace('-', '')
 
     @classmethod
-    def initialize(self, line):
-        self.__init__(self, line)
-        return self
+    def initialize(cls, line):
+        """public method to initialize the class"""
+        cls.__init__(cls, line)
+        return cls
 
 
-class data_rogers(data_credit_card):
+class DataRogers(DataCreditCard):
+    """class representing Rogers credit card data"""
     # ROGERS column
     # 0 "Date",
     # 1 "Posted Date",
@@ -82,12 +91,35 @@ class data_rogers(data_credit_card):
     payment_filter = ["CashBack / Remises", "PAYMENT, THANK YOU"]
 
     def __init__(self, line):
-        data_credit_card.__init__(self, line[1], line[7], line[12])
+        DataCreditCard.__init__(self, line[1], line[7], line[12])
         self.transaction_date = line[0]
         if self.description not in self.payment_filter and self.amount[0] != "-":
             self.purchase = True
 
     @classmethod
-    def initialize(self, line):
-        self.__init__(self, line)
-        return self
+    def initialize(cls, line):
+        """public method to initialize the class"""
+        cls.__init__(cls, line)
+        return cls
+
+class DataBmo(DataCreditCard):
+    """class representing BMO credit card data"""
+    # bmo column
+    # 0 Item #
+    # 1 Card #
+    # 2 Transaction Date
+    # 3 Posting Date
+    # 4 Transaction Amount
+    # 5 Description
+    payment_filter = ["CashBack / Remises", "PAYMENT, THANK YOU"]
+
+    def __init__(self, line):
+        DataCreditCard.__init__(self, line[3], line[5], line[4])
+        self.transaction_date = line[2]
+        self.purchase = (self.description not in self.payment_filter and self.amount[0] != "-")
+
+    @classmethod
+    def initialize(cls, line):
+        """public method to initialize the class"""
+        cls.__init__(cls, line)
+        return cls
--- finance/credit_card_parser/parse_credit_card_csv.py
+++ finance/credit_card_parser/parse_credit_card_csv.py
@@ -1,11 +1,14 @@
 # Copyright Michael Choi All Rights Reserved
+"""driver program to parse credit card csv data"""
 
 import argparse
 import csv
-import libs.cc_csv_data
+import libs.cc_csv_data as cc_data
 
 parser = argparse.ArgumentParser(description="Parse Credit Card csv file")
-parser.add_argument("company", help="Credit Card company", choices= ["brim", "mbna", "rogers"])
+parser.add_argument("company",
+                    help="Credit Card company",
+                    choices= ["brim", "mbna", "rogers", "bmo"])
 parser.add_argument("filename", help="Credit Card csv file, full path")
 args = parser.parse_args()
 
@@ -14,29 +17,40 @@ company = args.company
 
 match company:
     case "brim":
-        process_data = libs.cc_csv_data.data_brim.initialize
-        output_filename = "brim.csv"
-        output_data = libs.cc_csv_data.data_brim.display
+        process_data = cc_data.DataBrim.initialize
+        OUTPUT_FILENAME = "brim.csv"
+        output_data = cc_data.DataBrim.display
+        LINE_TO_SKIP = 1
     case "mbna":
-        process_data = libs.cc_csv_data.data_mbna.initialize
-        output_filename = "mbna.csv"
-        output_data = libs.cc_csv_data.data_mbna.display
+        process_data = cc_data.DataMbna.initialize
+        OUTPUT_FILENAME = "mbna.csv"
+        output_data = cc_data.DataMbna.display
+        LINE_TO_SKIP = 1
     case "rogers":
-        process_data = libs.cc_csv_data.data_rogers.initialize
-        output_filename = "rogers.csv"
-        output_data = libs.cc_csv_data.data_rogers.display
+        process_data = cc_data.DataRogers.initialize
+        OUTPUT_FILENAME = "rogers.csv"
+        output_data = cc_data.DataRogers.display
+        LINE_TO_SKIP = 1
+    case "bmo":
+        process_data = cc_data.DataBmo.initialize
+        OUTPUT_FILENAME = "bmo.csv"
+        output_data = cc_data.DataBmo.display
+        LINE_TO_SKIP = 3
 
 purchases = []
-with open(filelocation) as csvfile:
+with open(filelocation, encoding="utf-8") as csvfile:
     reader = csv.reader(csvfile)
-    next(reader)
+
+    # Skip the specified number of lines
+    for _ in range(LINE_TO_SKIP):
+        next(reader)
 
     for row in reader:
-        parsed = process_data(row)
-        if parsed.purchase:
-            purchases.append(output_data(parsed))
+        ParsedData = process_data(row)
+        if cc_data.DataCreditCard.is_purchase(ParsedData):
+            purchases.append(output_data(ParsedData))
 
-with open(output_filename, "w") as output:
+with open(OUTPUT_FILENAME, mode="w", encoding="utf-8") as output:
     for entry in purchases:
         output.write(entry)
         output.write("\n")
